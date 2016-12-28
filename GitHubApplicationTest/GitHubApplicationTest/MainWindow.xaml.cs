58557194bec8a2080c8576f4e2db708c4179f7ef
fernanda barba <fernanda.barba@wernerpegasus.com>
2016-12-28 16:02:38 -0600

    se agrega boton
    Se agrega boton para la funcionalidad X

--- GitHubApplicationTest/GitHubApplicationTest/MainWindow.xaml.cs
+++ GitHubApplicationTest/GitHubApplicationTest/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace GitHubApplicationTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hola");
+        }
     }
 }
