b1013d08f63de71a64b458a249751f8947849aaf
ferBarba <fernanda.barba@wernerpegasus.com>
2016-12-28 16:12:11 -0600

    update clasenueva.cs inserto error
    inserto un error en esta clase

--- GitHubApplicationTest/GitHubApplicationTest/ClaseNueva.cs
+++ GitHubApplicationTest/GitHubApplicationTest/ClaseNueva.cs
@@ -10,6 +10,7 @@ namespace GitHubApplicationTest
         public ClaseNueva()
         {
             //agrego el constructor desde github online
+            inserto un error!!!!
         }
     }
 }
